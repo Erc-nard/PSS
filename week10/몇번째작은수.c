fef53f4cd64705d95e8fc81317d30f89c5ec969f
Ercnard <100018037+Erc-nard@users.noreply.github.com>
2024-05-08 13:50:04 +0900

    Update 몇번째작은수.c

--- week10/몇번째작은수.c
+++ week10/몇번째작은수.c
@@ -1,21 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
 
-int partition(int *A, int p, int r) {
+int partition(int* A, int p, int r) {
     int i, j;
     int pivot;
     int t;
 
     i = p - 1;
     j = p;
-    
+
     //t = <- random int from (p,r)
     //swap A[t] <--> A[r]
     pivot = A[r];
 
-    for(j=p; j<r; j++) {
-        if(A[j] < pivot) {
+    for (j = p; j < r; j++) {
+        if (A[j] < pivot) {
             i++;
             //swap A[i] <--> A[j]
             t = A[i];
@@ -23,51 +24,52 @@ int partition(int *A, int p, int r) {
             A[j] = t;
         }
     }
-    i++; 
+    i++;
     //swap A[i] <--> A[r] 
-    t = A[i]; 
-    A[i] = A[r]; 
+    t = A[i];
+    A[i] = A[r];
     A[r] = t;
 
     return i;
 }
 
-void quick_sort(int *A, int p, int r,int num) {//if(num-q<0):¿ÞÂÊ else ¿À¸¥ÂÊ
-    for(int i = 0;i<10;i++){
-        printf("%d ",A[i]);
+void quick_sort(int* A, int p, int r, int num) {//if(num-q<0):ì™¼ìª½ else ì˜¤ë¥¸ìª½
+    for (int i = 0; i < 10; i++) {
+        printf("%d ", A[i]);
     }
     printf("\n");
-    
-    if(p<r&&num>=0) {
+
+    if (p < r && num >= 0) {
         int q = partition(A, p, r);
-        printf("²ó:%d\n",q);
-        if(num-q<0)
-        	quick_sort(A, p, q-1,num);
-        else if(num-q>0)
-        	quick_sort(A, q+1, r,num-p-q-1);
-        else printf("%d",A[num]);
-    } 
-    
+        printf("ë€¨:%d\n", q);
+        if (num - q < 0)
+            quick_sort(A, p, q - 1, num);
+        else
+            quick_sort(A, q + 1, r, num - p - q - 1);
+
+    }
+
 }
 
-int main(){
-        int num;
-        int randomnum;
-        srand(time(NULL));
-        
-        printf("Enter the number of numbers:");
-        scanf("%d",&randomnum);
-        printf("¸î ¹øÂ°·Î ÀÛÀº ¼ö:");
-        scanf("%d",&num);
-        
-        int *a = (int *)malloc(sizeof(int)*randomnum);
-        for(int i=0;i<randomnum;i++){
-            a[i]=rand()%101;
-            printf("%d ",a[i]);
-        }
-            printf("\n");
-            
-            quick_sort(a,0,randomnum-1,num-1);
-           // printf("%d",a[num-1]);
-            
+int main() {
+    int num;
+    int randomnum;
+    srand(time(NULL));
+
+    printf("Enter the number of numbers:");
+    scanf("%d", &randomnum);
+    printf("ëª‡ ë²ˆì§¸ë¡œ ìž‘ì€ ìˆ˜:");
+    scanf("%d", &num);
+
+    int* a = (int*)malloc(sizeof(int) * randomnum);
+    for (int i = 0; i < randomnum; i++) {
+        a[i] = rand() % 101;
+        printf("%d ", a[i]);
+    }
+    printf("\n");
+
+    quick_sort(a, 0, randomnum - 1, num - 1);
+    printf("%d", a[num-1]);
+    // printf("%d",a[num-1]);
+
 }

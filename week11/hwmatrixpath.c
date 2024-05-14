4041f057e3f95ea6dd01269a0ad83d949a2ba5b5
Ercnard <100018037+Erc-nard@users.noreply.github.com>
2024-05-14 15:33:45 +0900

    Update hwmatrixpath.c

--- week11/hwmatrixpath.c
+++ week11/hwmatrixpath.c
@@ -5,20 +5,25 @@ int matrixPath_memo(int **m, int r, int c, int i, int j, int **M,int *icount, in
 {
     if (i == 0 && j == 0) return m[0][0];
     else if (i == 0) {
-        if (M[0][j-1] == 0)
-            M[0][j-1] = matrixPath_memo(m,r,c,0,j-1,M);
+        if (/*M[0][j-1] == 0*/ *jcount<j-1){
+            *jcount++;
+            M[0][j-1] = matrixPath_memo(m,r,c,0,j-1,M,icount,jcount);
+            
+        }
         return M[0][j-1] + m[i][j];;
     }
     else if (j == 0) {
-        if (M[i-1][0] == 0)
-            M[i-1][0] = matrixPath_memo(m,r,c,i-1,0,M);
+        if (/*M[i-1][0] == 0*/ *icount<i-1){
+            *icount++;
+            M[i-1][0] = matrixPath_memo(m,r,c,i-1,0,M,icount,jcount);
+            }
         return M[i-1][0] + m[i][j];
     }
     else {
-        if (M[i-1][j] == 0)
-            M[i-1][j] = matrixPath_memo(m,r,c,i-1,j,M);
-        if (M[i][j-1] == 0)
-            M[i][j-1] = matrixPath_memo(m,r,c,i,j-1,M);
+        if (/*M[i-1][j] == 0*/ *icount<i-1&&*jcount<j)
+            M[i-1][j] = matrixPath_memo(m,r,c,i-1,j,M,icount+1,jcount+1);
+        if (/*M[i][j-1] == 0*/ *icount<i&&*jcount<j-1)
+            M[i][j-1] = matrixPath_memo(m,r,c,i,j-1,M,icount+1,jcount+1);
             return (M[i-1][j] < M[i][j-1] ? M[i-1][j]: M[i][j-1]) + m[i][j];
     }
 }
@@ -35,8 +40,8 @@ int main(void)
     }
     
     
+    
     int icount=0,jcount=0;
 
-    printf("%d\n", matrixPath_memo(m,r,c,3,3,M,*icount,*jcount));
+    printf("%d\n", matrixPath_memo(m,r,c,3,3,M,&icount,&jcount));
 }
-

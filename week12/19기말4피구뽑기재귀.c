2401e38530715830753be1bd8862485a8c4522fc
Erc-nard <100018037+Erc-nard@users.noreply.github.com>
2024-05-28 00:24:51 +0900

    hwdone

--- week12/19기말4피구뽑기재귀.c
+++ week12/19기말4피구뽑기재귀.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-void pick(int* student, int size, int* bucket, int bucketSize, int k,int tot,int*min)
-{
-	int smallest;
 
+int pick(int* student, int size, int* bucket, int bucketSize, int k,int tot)
+{
+	int smallest,result,last;
+	int min=tot+1;
+	//printf("bucketsize:%d k:%d\n",bucketSize,k);
 	if (k == 0) // trivial case
 	{
         int now=0;
@@ -12,36 +14,42 @@ void pick(int* student, int size, int* bucket, int bucketSize, int k,int tot,int
             //printf("%d ",student[bucket[i]]);
         }
         //printf("%d\n",now);
-        if(tot-now*2>0){
-            if(*min>tot-now*2) *min=tot-now*2;
-        }
-        else
-            if(*min>now*2-tot) *min=now*2-tot;
+		int all=tot-(now*2);
+
+		if(all<0 ) return (-all);
+		else return all;
+    
 	} 
+	last=bucketSize-k-1;
+	if(last==-1) smallest=0;
+	else smallest=bucket[last]+1;
 	// k > 0
-	for (int i = 0; i < size; i++)
-	{	int flag=0;
-		for(int j=0;j<=bucketSize-k;j++)
-			if(bucket[j]==i) flag=1;
-		if(flag==1) continue;
-		bucket[bucketSize-k] = i;
-		pick(student, size, bucket, bucketSize, k - 1,tot,min);
+	for (int i = smallest; i < size; i++)
+	{
+		bucket[last+1] = i;
+		result =pick(student, size, bucket, bucketSize, k - 1,tot);
+		if(min>result)
+			min=result;
 	}
+
+	return min;
 }
 
 int main() {
-	int num,min=100000;
+	int num,min;
 	int tot = 0;
 	scanf("%d", &num);
 	int* Student = (int*)malloc(sizeof(int) * num);
-    int* bucket = (int*)malloc(sizeof(int) * num);
-
+    int* bucket = (int*)malloc(sizeof(int) * (num/2));
+	
 	for (int i = 0; i < num; i++) {
 		scanf("%d", &Student[i]);
 		tot += Student[i];
 	}
-	pick(Student, num, bucket, num/2, num/2,tot,&min);
-    printf("%d",min);
+
+	min= pick(Student, num, bucket, num/2, (num/2),tot);
+	printf("%d",min);
+
     free(Student);
     free(bucket);
 }
